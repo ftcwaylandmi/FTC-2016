a0b705584ed02e5201106aa1a1adc44f7b5df94f
ftcwaylandmi <ftc@waylandunion.org>
2016-11-01 18:44:16 -0400

    clean up
    remove some lines

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwarePushbot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwarePushbot.java
@@ -93,29 +93,6 @@ public class HardwarePushbot
         }
     }
 
-
-    /*
-    {
-        DriveForward(1);
-                DriveForwardTime(1, 4000);
-                TurnLeftTime(1, 500);
-        motorLeft.setPower(-1);
-        motorRight.setpower(1);
-        Thread.sleep(500);
-        DriveForward(1);
-        Thread.sleep(4000);
-    }
-
-
-
-
-
-
-        // Reset the cycle clock for the next pass.
-        period.reset();
-    }
-     */
-
     public void DriveForward(int i) {
         leftMotor.setPower(-1);
         rightMotor.setPower(1);
